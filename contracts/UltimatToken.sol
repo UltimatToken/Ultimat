62110e76d87bdbfb3cd63d8bcaf079035d6ecd45
Ultimat <33989392+UltimatToken@users.noreply.github.com>
2017-11-26 12:34:05 +0100

    Update UltimatToken.sol

--- contracts/UltimatToken.sol
+++ contracts/UltimatToken.sol
@@ -2,24 +2,7 @@ pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract Owned {
-    address public owner;
-
-    function owned() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-}
-    
-contract Ultimat is Owned {
+contract Ultimat {
     // Public variables of the token
     string public name;
     string public symbol;
